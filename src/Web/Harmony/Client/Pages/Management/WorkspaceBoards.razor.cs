ea20913ae43074d30bd3b01363d2f678b2370012
chsakell <chsakell@gmail.com>
2023-09-30 17:28:08 +0300

    add workspace members and boards pages

---
+++ src/Web/Harmony/Client/Pages/Management/WorkspaceBoards.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Harmony.Client.Pages.Management
+{
+    public partial class WorkspaceBoards
+    {
+        [Parameter]
+        public string Id { get; set; }
+
+        [Parameter]
+        public string Name { get; set; }
+    }
+}
---
+++ src/Web/Harmony/Client/Pages/Management/WorkspaceMembers.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Harmony.Client.Pages.Management
+{
+    public partial class WorkspaceMembers
+    {
+        [Parameter]
+        public string Id { get; set; }
+
+        [Parameter]
+        public string Name { get; set; }
+    }
+}
