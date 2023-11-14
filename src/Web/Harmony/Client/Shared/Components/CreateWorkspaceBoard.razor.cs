aed70bf1d5308cb1880139adc6cc414ce73dd60a
chsakell <chsakell@gmail.com>
2023-11-14 20:48:50 +0200

    minor fixes

--- src/Web/Harmony/Client/Shared/Components/CreateWorkspaceBoard.razor.cs
+++ src/Web/Harmony/Client/Shared/Components/CreateWorkspaceBoard.razor.cs
@@ -1,4 +1,6 @@
-﻿using Harmony.Client.Shared.Modals;
+﻿using Harmony.Application.DTO;
+using Harmony.Client.Shared.Modals;
+using Harmony.Shared.Utilities;
 using MudBlazor;
 
 namespace Harmony.Client.Shared.Components
@@ -6,11 +8,21 @@ namespace Harmony.Client.Shared.Components
     public partial class CreateWorkspaceBoard
     {
         
-        private void OpenCreateWorkspaceModal()
+        private async Task OpenCreateWorkspaceModal()
         {
             var parameters = new DialogParameters();
             var options = new DialogOptions { CloseButton = true, MaxWidth = MaxWidth.Small, FullWidth = true, DisableBackdropClick = true };
-            _dialogService.Show<CreateWorkspaceModal>(_localizer["Create Workspace"], parameters, options);
+            var dialog = _dialogService.Show<CreateWorkspaceModal>(_localizer["Create Workspace"], parameters, options);
+
+            var result = await dialog.Result;
+            if (!result.Canceled)
+            {
+                var workspace = result.Data as WorkspaceDto;
+                if (workspace != null)
+                {
+                    Navigate(workspace);
+                }
+            }
         }
 
         private void OpenCreateBoardModal()
@@ -18,6 +30,17 @@ namespace Harmony.Client.Shared.Components
             var parameters = new DialogParameters();
             var options = new DialogOptions { CloseButton = true, MaxWidth = MaxWidth.Small, FullWidth = true, DisableBackdropClick = true };
             _dialogService.Show<CreateBoardModal>(_localizer["Create Board"], parameters, options);
+        }
+
+        private void Navigate(WorkspaceDto workspace)
+        {
+            if (string.IsNullOrEmpty(workspace?.Name))
+            {
+                return;
+            }
+
+            var slug = StringUtilities.SlugifyString(workspace.Name);
+            _navigationManager.NavigateTo($"workspaces/{workspace.Id}/{slug}");
         }
     }
 }
