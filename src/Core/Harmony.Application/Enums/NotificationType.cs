7629eb3dbcce28f48e46985cf6c60874289580c1
chsakell <chsakell@gmail.com>
2023-12-02 15:06:22 +0200

    add more notification types

--- src/Core/Harmony.Application/Enums/NotificationType.cs
+++ src/Core/Harmony.Application/Enums/NotificationType.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Harmony.Application.Enums
+﻿namespace Harmony.Application.Enums
 {
     public enum NotificationType
     {
         CardDueDateUpdated,
         CardCompleted,
+        MemberAddedToCard,
+        MemberRemovedFromCard,
+        CommentAddedToCard,
+        AttachmentAddedToCard,
+        CardAddedToBoard,
         MemberAddedToBoard,
+        MemberRemovedFromBoard,
+        MemberAddedToWorkspace,
+        MemberRemovedFromWorkspace
     }
 }
