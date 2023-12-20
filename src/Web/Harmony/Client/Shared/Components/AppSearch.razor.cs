087810b4ef21c782be62607a1adab37af68aa488
chsakell <chsakell@gmail.com>
2023-12-20 07:27:26 +0200

    add app bar search

---
+++ src/Web/Harmony/Client/Shared/Components/AppSearch.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace Harmony.Client.Shared.Components
+{
+    public partial class AppSearch
+    {
+        private string _searchText { get; set; }
+
+        private async Task<IEnumerable<string>> Search(string value, CancellationToken token)
+        {
+            //the http endpoint does not return immediately. There is an artifical delay built-in
+            return new List<string>() {  "item 1", "item 2"};
+        }
+
+        //private async Task Search(string searchText)
+        //{
+
+        //}
+    }
+}
