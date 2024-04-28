f4b99a79595f8c9a8484c8970ac5b8d324bcecd9
chsakell <chsakell@gmail.com>
2024-04-28 14:43:56 +0300

    fix concurrency webhooks issue

--- src/Integrations/Harmony.Integrations.SourceControl/Controllers/GithubController.cs
+++ src/Integrations/Harmony.Integrations.SourceControl/Controllers/GithubController.cs
@@ -9,8 +9,10 @@ using Harmony.Integrations.SourceControl.Constants;
 using Harmony.Integrations.SourceControl.WebhookRequests.GitHub;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Concurrent;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Threading;
 
 namespace Harmony.Integrations.SourceControl.Controllers
 {
@@ -18,7 +20,12 @@ namespace Harmony.Integrations.SourceControl.Controllers
     {
         private readonly IMediator _mediator;
         private readonly ILogger<GithubController> _logger;
-        private static char[] ALLOWED_CHARS = [' ','_','-','/'];
+        private static char[] ALLOWED_CHARS = [' ', '_', '-', '/'];
+        private static int MAX_CONCURRENCY = 1;
+        private static int SEMAPHORE_TIMEOUT_MIL = 3000;
+
+        private static ConcurrentDictionary<string, SemaphoreSlim> _semaphores =
+                new ConcurrentDictionary<string, SemaphoreSlim>();
         public GithubController(
             IMediator mediator,
             ILogger<GithubController> logger)
@@ -77,19 +84,24 @@ namespace Harmony.Integrations.SourceControl.Controllers
                 switch (eventType)
                 {
                     case "create":
-                        await _mediator.Send(new CreateBranchCommand()
+                        var serialKey = GetSerialKey(request.Ref);
+
+                        await RunWithLock(serialKey, nameof(HandleEventWebhook), async () =>
                         {
-                            SerialKey = GetSerialKey(request.Ref),
-                            Name = request.Ref,
-                            Repository = new Repository()
+                            await _mediator.Send(new CreateBranchCommand()
                             {
-                                RepositoryId = request.Repository.Id.ToString(),
-                                Name = request.Repository.Name,
-                                FullName = request.Repository.FullName,
-                                Url = request.Repository.HtmlUrl,
-                                Provider = SourceControlProvider.GitHub
-                            },
-                            Creator = new RepositoryUser(request.Sender.Login, request.Sender.AvatarUrl, request.Sender.HtmlUrl)
+                                SerialKey = serialKey,
+                                Name = request.Ref,
+                                Repository = new Repository()
+                                {
+                                    RepositoryId = request.Repository.Id.ToString(),
+                                    Name = request.Repository.Name,
+                                    FullName = request.Repository.FullName,
+                                    Url = request.Repository.HtmlUrl,
+                                    Provider = SourceControlProvider.GitHub
+                                },
+                                Creator = new RepositoryUser(request.Sender.Login, request.Sender.AvatarUrl, request.Sender.HtmlUrl)
+                            });
                         });
                         break;
                     case "delete":
@@ -105,79 +117,19 @@ namespace Harmony.Integrations.SourceControl.Controllers
             }
         }
 
+
         private async Task HandlePushWebhook(string postData)
         {
             var request = JsonSerializer.Deserialize<GitHubPushRequest>(postData);
             var branch = request.Ref.Replace("refs/heads/", string.Empty);
+            var serialKey = GetSerialKey(branch);
 
-            var push = new CreatePushCommand()
+            await RunWithLock(serialKey, nameof(HandlePushWebhook), async () =>
             {
-                SerialKey = GetSerialKey(branch),
-                Branch = branch,
-                Repository = new Repository()
+                var push = new CreatePushCommand()
                 {
-                    RepositoryId = request.Repository.Id.ToString(),
-                    Name = request.Repository.Name,
-                    FullName = request.Repository.FullName,
-                    Url = request.Repository.HtmlUrl,
-                    Provider = SourceControlProvider.GitHub
-                },
-                Commits = request.Commits.Select(commit =>
-                    new Commit()
-                    {
-                        Url = commit.Url,
-                        Author = new Author()
-                        {
-                            Email = commit.Author.Email,
-                            Name = commit.Author.Name,
-                            Username = commit.Author.Username,
-                        },
-                        Id = commit.Id,
-                        Message = commit.Message,
-                        Timestamp = commit.Timestamp,
-                        Added = commit.Added ?? new List<string>(),
-                        Removed = commit.Removed ?? new List<string>(),
-                        Modified = commit.Modified ?? new List<string>(),
-                    }).ToList(),
-                Sender = new RepositoryUser(request.Sender.Login, request.Sender.AvatarUrl, request.Sender.HtmlUrl)
-            };
-
-            await _mediator.Send(push);
-        }
-
-        private async Task HandlePullRequestWebhook(string postData)
-        {
-            var request = JsonSerializer.Deserialize<GitHubPullRequest>(postData);
-
-            if (request != null)
-            {
-                var pullRequest = new CreatePullRequestCommand()
-                {
-                    SerialKey = GetSerialKey(request.PullRequest.Head.Ref),
-                    Id = request.PullRequest.Id.ToString(),
-                    Action = request.Action,
-                    HtmlUrl = request.PullRequest.HtmlUrl,
-                    DiffUrl = request.PullRequest.DiffUrl,
-                    State = request.PullRequest.State
-                    switch
-                    {
-                        "opened" => PullRequestState.Opened,
-                        "closed" => request.PullRequest.MergedAt.HasValue ? PullRequestState.Merged : PullRequestState.Closed,
-                        _ => PullRequestState.Opened
-                    },
-                    Title = request.PullRequest.Title,
-                    Number = request.PullRequest.Number,
-                    CreatedAt = request.PullRequest.CreatedAt,
-                    UpdatedAt = request.PullRequest.UpdatedAt,
-                    ClosedAt = request.PullRequest.ClosedAt,
-                    MergedAt = request.PullRequest.MergedAt,
-                    SourceBranch = request.PullRequest.Head.Ref,
-                    TargetBranch = request.PullRequest.Base.Ref,
-                    MergeCommitSha = request.PullRequest.MergeCommitSha,
-                    Assignees = request.PullRequest.Assignees.Select(a =>
-                        new RepositoryUser(a.Login, a.AvatarUrl, a.HtmlUrl)).ToList(),
-                    Reviewers = request.PullRequest.RequestedReviewers.Select(a =>
-                        new RepositoryUser(a.Login, a.AvatarUrl, a.HtmlUrl)).ToList(),
+                    SerialKey = serialKey,
+                    Branch = branch,
                     Repository = new Repository()
                     {
                         RepositoryId = request.Repository.Id.ToString(),
@@ -186,10 +138,114 @@ namespace Harmony.Integrations.SourceControl.Controllers
                         Url = request.Repository.HtmlUrl,
                         Provider = SourceControlProvider.GitHub
                     },
+                    Commits = request.Commits.Select(commit =>
+                        new Commit()
+                        {
+                            Url = commit.Url,
+                            Author = new Author()
+                            {
+                                Email = commit.Author.Email,
+                                Name = commit.Author.Name,
+                                Username = commit.Author.Username,
+                            },
+                            Id = commit.Id,
+                            Message = commit.Message,
+                            Timestamp = commit.Timestamp,
+                            Added = commit.Added ?? new List<string>(),
+                            Removed = commit.Removed ?? new List<string>(),
+                            Modified = commit.Modified ?? new List<string>(),
+                        }).ToList(),
                     Sender = new RepositoryUser(request.Sender.Login, request.Sender.AvatarUrl, request.Sender.HtmlUrl)
                 };
 
-                await _mediator.Send(pullRequest);
+                await _mediator.Send(push);
+            });
+        }
+
+        private async Task HandlePullRequestWebhook(string postData)
+        {
+            var request = JsonSerializer.Deserialize<GitHubPullRequest>(postData);
+
+            if (request != null)
+            {
+                var serialKey = GetSerialKey(request.PullRequest.Head.Ref);
+
+                await RunWithLock(serialKey, nameof(HandlePullRequestWebhook), async () =>
+                {
+                    var pullRequest = new CreatePullRequestCommand()
+                    {
+                        SerialKey = serialKey,
+                        Id = request.PullRequest.Id.ToString(),
+                        Action = request.Action,
+                        HtmlUrl = request.PullRequest.HtmlUrl,
+                        DiffUrl = request.PullRequest.DiffUrl,
+                        State = request.PullRequest.State
+                    switch
+                        {
+                            "opened" => PullRequestState.Opened,
+                            "closed" => request.PullRequest.MergedAt.HasValue ? PullRequestState.Merged : PullRequestState.Closed,
+                            _ => PullRequestState.Opened
+                        },
+                        Title = request.PullRequest.Title,
+                        Number = request.PullRequest.Number,
+                        CreatedAt = request.PullRequest.CreatedAt,
+                        UpdatedAt = request.PullRequest.UpdatedAt,
+                        ClosedAt = request.PullRequest.ClosedAt,
+                        MergedAt = request.PullRequest.MergedAt,
+                        SourceBranch = request.PullRequest.Head.Ref,
+                        TargetBranch = request.PullRequest.Base.Ref,
+                        MergeCommitSha = request.PullRequest.MergeCommitSha,
+                        Assignees = request.PullRequest.Assignees.Select(a =>
+                            new RepositoryUser(a.Login, a.AvatarUrl, a.HtmlUrl)).ToList(),
+                        Reviewers = request.PullRequest.RequestedReviewers.Select(a =>
+                            new RepositoryUser(a.Login, a.AvatarUrl, a.HtmlUrl)).ToList(),
+                        Repository = new Repository()
+                        {
+                            RepositoryId = request.Repository.Id.ToString(),
+                            Name = request.Repository.Name,
+                            FullName = request.Repository.FullName,
+                            Url = request.Repository.HtmlUrl,
+                            Provider = SourceControlProvider.GitHub
+                        },
+                        Sender = new RepositoryUser(request.Sender.Login, request.Sender.AvatarUrl, request.Sender.HtmlUrl)
+                    };
+
+                    await _mediator.Send(pullRequest);
+                });
+            }
+        }
+
+        private async Task RunWithLock(string serialKey, string funcName, Func<Task> func)
+        {
+            SemaphoreSlim semaphore = null;
+
+            try
+            {
+                if (!string.IsNullOrWhiteSpace(serialKey))
+                {
+                    semaphore = new SemaphoreSlim(MAX_CONCURRENCY);
+                    semaphore = _semaphores.GetOrAdd(serialKey, semaphore);
+                    var accessGranted = await semaphore.WaitAsync(SEMAPHORE_TIMEOUT_MIL);
+
+                    if (!accessGranted)
+                    {
+                        _logger.LogWarning($"Couldn't acquire access for {funcName}");
+                    }
+                }
+
+                await func();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error handling {funcName}");
+            }
+            finally
+            {
+                if (semaphore != null)
+                {
+                    semaphore.Release();
+                    _semaphores.TryRemove(serialKey, out _);
+                }
             }
         }
 
@@ -199,15 +255,15 @@ namespace Harmony.Integrations.SourceControl.Controllers
                        .Cast<Match>()
                        .Select(x => x.Value).ToList();
 
-            foreach(var match in matches)
+            foreach (var match in matches)
             {
                 var matchIndex = text.IndexOf(match, StringComparison.InvariantCultureIgnoreCase);
 
-                if(matchIndex == 0 && NextCharAllowed(text, match, matchIndex))
+                if (matchIndex == 0 && NextCharAllowed(text, match, matchIndex))
                 {
                     return match;
                 }
-                else if(matchIndex > 0)
+                else if (matchIndex > 0)
                 {
                     var previousChar = text[matchIndex - 1];
                     if (PreviewCharAllowed(text, matchIndex) && NextCharAllowed(text, match, matchIndex))
@@ -234,7 +290,7 @@ namespace Harmony.Integrations.SourceControl.Controllers
 
         private bool NextCharAllowed(string original, string term, int termIndex)
         {
-            if(original.Length == term.Length + termIndex)
+            if (original.Length == term.Length + termIndex)
             {
                 return true;
             }
